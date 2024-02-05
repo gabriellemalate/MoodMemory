8d77192f3978855560d30869c2105cb58716ac28
Gabrielle Malate <gabriellemalate@gmail.com>
2024-02-04 19:44:28 -0800

    desktop screen refine design

--- src/pages/WelcomePage/WelcomePage.js
+++ src/pages/WelcomePage/WelcomePage.js
@@ -1,9 +1,9 @@
 import React from "react";
 import "./WelcomePage.scss";
-import LoadingPage from "../LoadingPage/LoadingPage";
-// import { Link } from "react-router-dom";
 import { auth } from "../../firebase";
 import { GoogleAuthProvider, signInWithRedirect } from "firebase/auth";
+// import { getFirestore, doc, setDoc } from "firebase/firestore";
+// import { functions} from "firebase/functions";
 
 const WelcomePage = () => {
     const googleSignIn = () => {
@@ -11,16 +11,32 @@ const WelcomePage = () => {
         signInWithRedirect(auth, provider);
     };
 
+
+    // const db = getFirestore();
+
+    // const createUserData = functions.auth.user().onCreate(async (user) => {
+    //     const uid = user.uid;
+
+    //     // Create a new collection for the user in Firestore
+    //     const userCollectionRef = doc(db, "users", uid);
+    //     await setDoc(userCollectionRef, {
+    //         // Initialize user-specific data here
+    //         displayName: user.displayName,
+    //         email: user.email,
+
+    //     });
+    // });
+
     return (
         <main className="welcome">
             <article className='morph-wrap'>
-                    <div className="morphs_1 morphs" />
-                    <div className="morphs_2 morphs" />
-                </article>
-                
+                <div className="morphs_1 morphs" />
+                <div className="morphs_2 morphs" />
+            </article>
+
             <div className="welcome-eq">
 
-                
+
 
                 <div className="welcome__head">
                     <h1 className="welcome__head-welcome">welcome to</h1>
