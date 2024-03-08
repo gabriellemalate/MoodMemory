cf279f662d97d670e6eba27b7a0834306cfd6a25
Gabrielle Malate <gabriellemalate@gmail.com>
2024-03-08 07:51:16 -0800

    Var for sorts

--- src/components/Logged/Logged.js
+++ src/components/Logged/Logged.js
@@ -37,43 +37,46 @@ function Logged({ logData, searchTerm }) {
         return lowercaseTitle.includes(lowercaseSearchTerm) || lowercaseEmotion.includes(lowercaseSearchTerm) || lowercaseLevel.includes(lowercaseSearchTerm);
     };
 
+    // Sort logData by date from most recent to oldest
+    const sortedLogData = [...logData].sort((a, b) => b.date.toMillis() - a.date.toMillis());
+
     return (
         <>
             {sortedLogData.map((entry) => (
                 isMatch(entry) && (
-                <article
-                    key={entry.id} 
-                    className={`logged ${expanded ? 'expanded' : 'compressed'}`}>
-                    <div className='logged-eq' onClick={toggleExpand}>
+                    <article
+                        key={entry.id}
+                        className={`logged ${expanded ? 'expanded' : 'compressed'}`}>
+                        <div className='logged-eq' onClick={toggleExpand}>
 
-                        <div className='logged__frame'>
-                            <img className='logged__frame-emoji' alt='logged emotion' src={require(`../../assets/emotes/${logData.emoji}.png`)} />
-                        </div>
-                        <div className='logged-info'>
-                            <div className='logged-info__top'>
-                                <div className='logged-info__top-left'>
-                                    <h2 className='logged-info__top-left-emotion'>{logData.emotion}</h2>
-                                    <h3 className='logged-info__top-left-title'>{logData.title}</h3>
-                                </div>
-                                <div className='logged-info__top-right'>
-                                    <h2 className='logged-info__top-right-date'>{logData.date && logData.date.toDate().toLocaleDateString()}</h2>
-                                </div>
+                            <div className='logged__frame'>
+                                <img className='logged__frame-emoji' alt='logged emotion' src={require(`../../assets/emotes/${logData.emoji}.png`)} />
                             </div>
-                            <div className='logged-info__bottom'>
-                                <div className='logged-info__bottom-eq'>
-                                    <h2 className='logged-info__bottom-state'>{logData.state} {logData.level}</h2>
-                                    <div className='logged-info__bottom-lower'>
-                                        <span className='logged-info__bottom-lower-item'>Irr:<b className='logged-info--bold'>{logData.irritability}</b></span>
-                                        <span className='logged-info__bottom-lower-item'>Anx:<b className='logged-info--bold'>{logData.anxiety}</b></span>
-                                        <span className='logged-info__bottom-lower-item'> Hours:<b className='logged-info--bold'>{logData.hours}</b></span>
+                            <div className='logged-info'>
+                                <div className='logged-info__top'>
+                                    <div className='logged-info__top-left'>
+                                        <h2 className='logged-info__top-left-emotion'>{logData.emotion}</h2>
+                                        <h3 className='logged-info__top-left-title'>{logData.title}</h3>
+                                    </div>
+                                    <div className='logged-info__top-right'>
+                                        <h2 className='logged-info__top-right-date'>{logData.date && logData.date.toDate().toLocaleDateString()}</h2>
+                                    </div>
+                                </div>
+                                <div className='logged-info__bottom'>
+                                    <div className='logged-info__bottom-eq'>
+                                        <h2 className='logged-info__bottom-state'>{logData.state} {logData.level}</h2>
+                                        <div className='logged-info__bottom-lower'>
+                                            <span className='logged-info__bottom-lower-item'>Irr:<b className='logged-info--bold'>{logData.irritability}</b></span>
+                                            <span className='logged-info__bottom-lower-item'>Anx:<b className='logged-info--bold'>{logData.anxiety}</b></span>
+                                            <span className='logged-info__bottom-lower-item'> Hours:<b className='logged-info--bold'>{logData.hours}</b></span>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
-                    {expanded && <LoggedExpand logData={logData} />}
-                </article >
-            )
+                        {expanded && <LoggedExpand logData={logData} />}
+                    </article >
+                )
             ))}
         </>
     );
