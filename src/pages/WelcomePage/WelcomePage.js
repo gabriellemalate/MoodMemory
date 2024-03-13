c6232a2a60588096ceaf84ceb600a6d5d9774458
Gabrielle Malate <gabriellemalate@gmail.com>
2024-03-13 13:15:27 -0700

    Adjust imports welcome page

--- src/pages/WelcomePage/WelcomePage.js
+++ src/pages/WelcomePage/WelcomePage.js
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from "react";
 import "./WelcomePage.scss";
 import { auth } from "../../firebase";
-import { GoogleAuthProvider, signInWithRedirect, onAuthStateChanged, signInWithPopup } from "firebase/auth";
-import { getFirestore, doc, setDoc, collection, getDoc, addDoc } from "firebase/firestore";
-import { functions } from "firebase/functions";
-import { navigate } from "react-router-dom";
+import { GoogleAuthProvider, signInWithRedirect, onAuthStateChanged
+    // , signInWithPopup 
+} from "firebase/auth";
+// import { getFirestore, doc, setDoc, collection, getDoc, addDoc } from "firebase/firestore";
+// import { functions } from "firebase/functions";
+// import { navigate } from "react-router-dom";
 import LoadingPage from "../LoadingPage/LoadingPage";
 
 const WelcomePage = () => {
