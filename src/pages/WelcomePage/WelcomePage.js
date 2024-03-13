40eab8c7471ff077c46398d4706b37e83fa5c7c2
Gabrielle Malate <gabriellemalate@gmail.com>
2024-03-13 15:56:25 -0700

    WElcome imports

--- src/pages/WelcomePage/WelcomePage.js
+++ src/pages/WelcomePage/WelcomePage.js
@@ -10,71 +10,72 @@ import { GoogleAuthProvider, signInWithRedirect, onAuthStateChanged
 import LoadingPage from "../LoadingPage/LoadingPage";
 
 const WelcomePage = () => {
-    const [redirectInProgress, setRedirectInProgress] = useState(false);
+    // const [redirectInProgress, setRedirectInProgress] = useState(false);
 
-    useEffect(() => {
-        // Check if there is a redirect result when the component mounts
-        getRedirectResult(auth)
-            .then((result) => {
-                if (result.user) {
-                    // User is already signed in, redirect them to home page
-                    window.location.href = '/moodhome';
-                }
-            })
-            .catch((error) => {
-                console.error("Error getting redirect result:", error);
-            });
-    }, []);
-
-    const googleSignIn = () => {
-        const provider = new GoogleAuthProvider();
-
-        // Set a flag to indicate redirect is in progress
-        setRedirectInProgress(true);
-
-        // Initiate Google sign-in with redirect
-        signInWithRedirect(auth, provider)
-            .catch((error) => {
-                console.error("Error signing in with Google:", error);
-                // Reset the redirect flag in case of error
-                setRedirectInProgress(false);
-            });
-    };
+    // useEffect(() => {
+    //     // Check if there is a redirect result when the component mounts
+    //     getRedirectResult(auth)
+    //         .then((result) => {
+    //             if (result.user) {
+    //                 // User is already signed in, redirect them to home page
+    //                 window.location.href = '/moodhome';
+    //             }
+    //         })
+    //         .catch((error) => {
+    //             console.error("Error getting redirect result:", error);
+    //         });
+    // }, []);
 
     // const googleSignIn = () => {
     //     const provider = new GoogleAuthProvider();
 
-    //     signInWithPopup(auth, provider)
-    //         .then((result) => {
-    //             // If the sign-in was successful, create a new document for the user
-    //             const user = result.user;
-    //             const db = getFirestore();
-    //             const userDocRef = doc(db, "users", user.uid);
-    //             return setDoc(userDocRef, {
-    //                 displayName: user.displayName,
-    //                 email: user.email,
-    //                 // any other initial user-specific data
-    //             });
-    //         })
-    //         .then(() => {
-    //             // Create moodlogs collection within the user's document
-    //             const moodLogsCollectionRef = collection(userDocRef, "moodlogs");
-    //             // Log the user's UID and email
-    //             console.log("User UID:", user.uid);
-    //             console.log("User Email:", user.email);
-    //             // navigate("/moodhome");
-    //         })
+    //     // Set a flag to indicate redirect is in progress
+    //     setRedirectInProgress(true);
+
+    //     // Initiate Google sign-in with redirect
+    //     signInWithRedirect(auth, provider)
     //         .catch((error) => {
     //             console.error("Error signing in with Google:", error);
+    //             // Reset the redirect flag in case of error
+    //             setRedirectInProgress(false);
     //         });
     // };
 
+    const googleSignIn = () => {
+        const provider = new GoogleAuthProvider();
+
+        // signInWithPopup
+        signInWithRedirect(auth, provider)
+            .then((result) => {
+                // If the sign-in was successful, create a new document for the user
+                const user = result.user;
+                const db = getFirestore();
+                const userDocRef = doc(db, "users", user.uid);
+                return setDoc(userDocRef, {
+                    displayName: user.displayName,
+                    email: user.email,
+                    // any other initial user-specific data
+                });
+            })
+            .then(() => {
+                // Create moodlogs collection within the user's document
+                const moodLogsCollectionRef = collection(userDocRef, "moodlogs");
+                // Log the user's UID and email
+                console.log("User UID:", user.uid);
+                console.log("User Email:", user.email);
+                // navigate("/moodhome");
+            })
+            .catch((error) => {
+                console.error("Error signing in with Google:", error);
+            });
+    };
+
     return (
         <main className="welcome">
-            {redirectInProgress ? (
+            {/* {redirectInProgress ? (
                 <LoadingPage />
-            ) : (
-                <>
+            ) : ( */}
+                {/* <> */}
                     <article className='morph-wrap'>
                         <div className="morphs_1 morphs" />
                         <div className="morphs_2 morphs" />
@@ -99,8 +100,8 @@ const WelcomePage = () => {
                             Sign In
                         </button>
                     </div>
-                </>
-            )}
+                {/* </>
+            )} */}
         </main>
     );
 }
