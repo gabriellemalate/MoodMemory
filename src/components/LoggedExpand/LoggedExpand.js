7426090f68f41136431f20668ca7bf6ff262d3eb
Gabrielle Malate <gabriellemalate@gmail.com>
2024-05-20 10:33:01 -0700

    resolve conflict merge

--- src/components/LoggedExpand/LoggedExpand.js
+++ src/components/LoggedExpand/LoggedExpand.js
@@ -75,7 +75,6 @@ function LoggedExpand({ logData }) {
         }
     };
 
-<<<<<<< HEAD
     const handleDeleteComment = async (commentId) => {
         const confirmDelete = window.confirm('Are you sure you want to delete this comment?');
 
@@ -136,29 +135,6 @@ function LoggedExpand({ logData }) {
 
                 <div className='open-eq'
                 >
-=======
-    useEffect(() => {
-        const q = query(collection(db, 'moodlogs'));
-        const unsubscribe = onSnapshot(q, (querySnapshot) => {
-            const data = [];
-            querySnapshot.forEach((doc) => {
-                data.push({ ...doc.data(), id: doc.id });
-            });
-            // Sort logs by date in descending order (most recent first)
-            data.sort((a, b) => b.date.toMillis() - a.date.toMillis());
-
-            setLogData(data);
-        });
-
-        return () => unsubscribe();
-    }, []);
-
-    return (
-        <>
-            <article key={entry.id} className="open">
-                
-                <div className='open-eq' onClick={toggleExpand}>
->>>>>>> develop
                     <div className='open-top'>
                         <div className='open-top-left'>
                             <div className='open-top-left__frame'>
@@ -168,13 +144,8 @@ function LoggedExpand({ logData }) {
                         </div>
                         <div className='open-top-right'>
                             <div className='open-top-right__stamp'>
-<<<<<<< HEAD
                                 <h3 className='open-top-right__date'>{formattedDate} </h3>
                                 <h3 className='open-top-right__time'>{time}</h3>
-=======
-                                <h3 className='open-top-right__date'>{logData.date && logData.date.toDate().toLocaleDateString()}</h3>
-
->>>>>>> develop
                             </div>
                             <h2 className='open-top-right__state'>{logData.state} {logData.level}</h2>
                         </div>
@@ -196,11 +167,7 @@ function LoggedExpand({ logData }) {
                             </div>
                         </div>
                         <div className='open-bottom-right'>
-<<<<<<< HEAD
                             <p className='open-bottom-right__hours'>{logData.hours} hr{logData.hours !== 1 ? 's' : ''}</p>
-=======
-                            <p className='open-bottom-right__hours'>{logData.hours} hour{logData.hours !== 1 ? 's' : ''}</p>
->>>>>>> develop
                             <img className='open-bottom-right__quality' src={require(`../../assets/${logData.quality}.png`)} alt="sleep quality" title="sleep quality" />
                         </div>
                     </div>
@@ -278,7 +245,6 @@ function LoggedExpand({ logData }) {
                             <button className='open-add__button' type="button" onClick={handleAddComment}>add</button>
                         </div>
                     </form>
-<<<<<<< HEAD
 
                     <button className='open__close' onClick={handleDelete}>
                         delete
@@ -295,14 +261,6 @@ function LoggedExpand({ logData }) {
                     <button onClick={handleCancelDelete}>Cancel</button>
                 </div>
             )}
-=======
-                    {/* <button className='open__close' onClick={toggleExpand}>
-                        close
-                    </button> */}
-                </div>
-            </article>
-
->>>>>>> develop
         </>
     );
 }
